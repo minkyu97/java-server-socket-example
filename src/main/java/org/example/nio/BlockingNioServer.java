397024cbdd7518232c9feb46c3265496b78e4637
Minkyu Lee <whizkyu@snu.ac.kr>
2024-08-22 00:52:01 +0900

    fix: PollingNioServer 비동기적으로 작동하도록 수정

--- src/main/java/org/example/nio/BlockingNioServer.java
+++ src/main/java/org/example/nio/BlockingNioServer.java
@@ -1,12 +1,13 @@
 package org.example.nio;
 
 import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 
-public class BlockingNioServer extends AbstractNioServer {
+public class BlockingNioServer {
     public static void main(String[] args) throws IOException {
         ServerSocketChannel serverSocket = ServerSocketChannel.open();
 
@@ -18,5 +19,33 @@ public class BlockingNioServer extends AbstractNioServer {
 
             handleRequest(socket, buffer);
         }
+    }
+
+    protected static void handleRequest(SocketChannel socket, ByteBuffer buffer) {
+        try {
+            while (socket.read(buffer) != -1) {
+                buffer.flip();
+
+                toUpperCase(buffer);
+
+                while (buffer.hasRemaining()) {
+                    socket.write(buffer);
+                }
+
+                buffer.compact();
+            }
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    private static void toUpperCase(ByteBuffer buffer) {
+        for (int i = 0; i < buffer.limit(); i++) {
+            buffer.put(i, (byte) toUpperCase(buffer.get(i)));
+        }
+    }
+
+    private static int toUpperCase(int data) {
+        return Character.isLetter(data) ? Character.toUpperCase(data) : data;
     }
 }
--- src/main/java/org/example/nio/PollingNioServer.java
+++ src/main/java/org/example/nio/PollingNioServer.java
@@ -1,6 +1,7 @@
 package org.example.nio;
 
 import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.ServerSocketChannel;
@@ -8,7 +9,7 @@ import java.nio.channels.SocketChannel;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
-public class PollingNioServer extends AbstractNioServer {
+public class PollingNioServer {
     public static void main(String[] args) throws IOException {
         ServerSocketChannel serverSocket = ServerSocketChannel.open();
 
@@ -34,5 +35,37 @@ public class PollingNioServer extends AbstractNioServer {
                 }
             });
         }
+    }
+
+    protected static void handleRequest(SocketChannel socket, ByteBuffer buffer) {
+        try {
+            int readBytes = socket.read(buffer);
+            if (readBytes == -1) {
+                System.out.println("Client disconnected: " + socket);
+                socket.close();
+            } else if (readBytes != 0) {
+                buffer.flip();
+
+                toUpperCase(buffer);
+
+                while (buffer.hasRemaining()) {
+                    socket.write(buffer);
+                }
+
+                buffer.compact();
+            }
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    protected static void toUpperCase(ByteBuffer buffer) {
+        for (int i = 0; i < buffer.limit(); i++) {
+            buffer.put(i, (byte) toUpperCase(buffer.get(i)));
+        }
+    }
+
+    private static int toUpperCase(int data) {
+        return Character.isLetter(data) ? Character.toUpperCase(data) : data;
     }
 }
